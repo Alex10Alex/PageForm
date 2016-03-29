f3ffaa4d964ceb94b78cc0ddb85283208d212e6e
alex <t0983781367@gmail.com>
2016-03-29 16:10:15 +0300

    add file

---
+++ newsletter/views.py
@@ -0,0 +1,73 @@
+from django.shortcuts import render
+from .forms import SignUpForm, ContactForm
+from django.core.mail import send_mail
+from django.conf import settings
+from .models import SignUp
+
+# Create your views here.
+def home(request):
+    title = 'Sign up now'
+    form = SignUpForm(request.POST or None)
+    context = {
+        "template_title": title,
+        "form": form
+    }
+    if form.is_valid():
+        instance = form.save(commit=False)
+        full_name = form.cleaned_data.get("full_name")
+        if not full_name:
+            full_name = "New full name"
+            instance.full_name = full_name
+        context = {
+            "template_title": "Thank you"}
+    if request.user.is_authenticated() and  request.user.is_staff:
+           # print(SignUp.objects.all)
+           # i = 1
+           # for instance in SignUp.objects.all():
+           #     print(i)
+           #     print(instance.full_name)
+           #     i+=1
+           queryset = SignUp.objects.all().order_by('-timestamp')#.filter(full_name__iexact = 'Djastin')
+           # print(SignUp.objects.all().order_by('-timestamp').filter(full_name__iexact = 'Djastin').count())
+           context = {
+               'queryset':queryset
+            }
+
+
+    return render(request, "home.html", context)
+
+
+def contact(request):
+    title = 'Contact Us'
+    title_align_center = True
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        # for key,value in form.cleaned_data.iteritems():
+        #     print key, value
+        # print form.cleaned_data.get(key)
+        form_email = form.cleaned_data.get("email")
+        form_message = form.cleaned_data.get("message")
+        form_full_name = form.cleaned_data.get("full_name")
+        # print email, message, full_name
+        subject = "Site contact form"
+        from_email = settings.EMAIL_HOST_USER
+        to_email = ['t098@list.ru']
+        contact_message = " %s: %s via %s" % (
+            form_full_name,
+            form_message,
+            form_email)
+        some_html_message = """
+        <h1>Hello</h1><h2>my dear </h2><h3>Freind</h3> """
+        send_mail(
+            subject,
+            contact_message,
+            from_email,
+            to_email,
+            html_message= some_html_message,
+            fail_silently=False)
+    context = {
+        "form": form,
+        "title": title,
+        "title_align_center":title_align_center,
+    }
+    return render(request, "forms.html", context)
